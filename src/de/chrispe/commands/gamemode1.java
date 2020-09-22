8d4d30d6c49a21ec2ff8ff26faaf84f94a977e6d
ChrisPEql <57588361+ChrisPEql@users.noreply.github.com>
2020-09-22 19:09:05 +0200

    Gamemode 3 Command hinzugefügt!

--- src/de/chrispe/commands/gamemode1.java
+++ src/de/chrispe/commands/gamemode1.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class gamemode1 implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+            System.out.println("Hello :3");
         return false;
     }
 }
