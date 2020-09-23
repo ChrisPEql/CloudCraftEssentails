2cf316fff4cbb776c113b68650f8c3c8e96f6783
ChrisPEql <57588361+ChrisPEql@users.noreply.github.com>
2020-09-23 08:14:43 +0200

    Sonstige Änderungen hinzugefügt!

--- src/de/chrispe/commands/gamemode2.java
+++ src/de/chrispe/commands/gamemode2.java
@@ -1,12 +1,17 @@
 package de.chrispe.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class gamemode2 implements CommandExecutor {
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
 }
+
